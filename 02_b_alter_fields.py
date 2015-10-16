efd049a818c5fb6c4ff6ee3636f079055af807a3
Nicolas Maire <nicolas.maire@unibas.ch>
2015-10-16 16:00:03 +0200

    added progress messages

--- 02_b_alter_fields.py
+++ 02_b_alter_fields.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
 import MySQLdb
 
-connection = MySQLdb.connect(host='localhost', user='data', passwd='data')
+
+host = 'localhost'
+user = 'data'
+pw = 'data'
+print("Connecting to mysql with following paramters:")
+print("Host: " + host)
+print("User: " + user)
+print("Password: " + pw)
+
+connection = MySQLdb.connect(host=host, user=user, passwd=pw)
 cursor = connection.cursor()
 cursor.execute("USE hrs_mysql_db")
 cursor.execute("SHOW TABLES")
 tables = cursor.fetchall()
+
 
 for table in tables:
     cursor.execute("SELECT COLUMN_NAME, DATA_TYPE FROM information_schema.columns WHERE "
                    "TABLE_SCHEMA='hrs_mysql_db' AND table_name = '" + table[0] + "'")
     fields = cursor.fetchall()
     for field in fields:
-        cursor.execute("ALTER TABLE hrs_mysql_db." + table[0] + " CHANGE " + field[0] + " " + field[0] + " VARCHAR(99)")
+        print("Altering table " + table[0] + ", field " + field[0])
+        query = "ALTER TABLE hrs_mysql_db." + table[0] + " CHANGE " + field[0] + " " + field[0] + " VARCHAR(99)"
+        try:
+            cursor.execute(query)
+        except:
+            print("unable to run query: " + query)
+
 
 
 
