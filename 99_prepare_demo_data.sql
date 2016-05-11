f26e9ddc4fad7d2e475734f382151d9e47e5ca9f
data data <you@me.com>
2016-05-11 11:44:01 +0200

    fixed demo data issue

--- 99_prepare_demo_data.sql
+++ 99_prepare_demo_data.sql
@@ -1,6 +1,6 @@
 use openhds;
 SELECT "CAUTION: this will truncate all tables in your openhds database, and update data in your hrs_mysql_db. If you really want to delete your data, remove line two in 99_prepare_demo_data.sql to proceed.";
-Remove this line to run the script;
+#Remove this line to run the script;
 SET FOREIGN_KEY_CHECKS=0;
 TRUNCATE TABLE AsyncTask;
 TRUNCATE TABLE adultvpm;
@@ -40,20 +40,17 @@ TRUNCATE TABLE whitelist;
 INSERT INTO `fieldworker` (`uuid`, `deleted`, `insertDate`, `voidDate`, `voidReason`, `extId`, `firstName`, `lastName`, `passwordHash`, `insertBy_uuid`, `voidBy_uuid`) VALUES
 ('ff808181547ae7c701547b78c1150012', 0, '2016-05-04', NULL, NULL, 'FWAD1', 'Admin', 'Data', '$2a$08$m/k/u4739d6wplA3hBpRE..lu7eTzl9nyAsXDKIQUW6NaWaUyxE5m', 'ff8081815462747d01546276bc260000', NULL);
 
-INSERT INTO `individual` VALUES ('Unknown Individual','\0','2009-12-19',NULL,NULL,'PENDING',NULL,'1900-12-19',NULL,'UNK','Unknown','MALE','UNKNOWN',NULL,'User 1',NULL,'UnknownFieldWorker',NULL,NULL);
-
+INSERT INTO `individual` VALUES ('Unknown Individual',0,'2009-12-19',NULL,NULL,'PENDING',NULL,'1900-12-19',NULL,'UNK','Unknown','MALE','UNKNOWN',NULL,NULL,'User 1',NULL,'UnknownFieldWorker',NULL,NULL);
 INSERT INTO `locationhierarchy` VALUES ('hierarchy1','SUI','Switzerland','hierarchyLevelId1','hierarchy_root'),('hierarchy2','NWS','NW','hierarchyLevelId2','hierarchy1'),('hierarchy3','BSL','Basel','hierarchyLevelId3','hierarchy2'),('hierarchy4','BSS','Basel-Stadt','hierarchyLevelId4','hierarchy3'),('hierarchy5_1','ISE','Iselin','hierarchyLevelId5','hierarchy4'),('hierarchy5_2','GTH','Gotthelf','hierarchyLevelId5','hierarchy4'),('hierarchy5_3','BCL','Bachletten','hierarchyLevelId5','hierarchy4'),('hierarchy5_4','GND','Gundeldingen','hierarchyLevelId5','hierarchy4'),('hierarchy_root','HIERARCHY_ROOT','',NULL,NULL);
 
 INSERT INTO `locationhierarchylevel` VALUES ('hierarchyLevelId1',1,'Country'),('hierarchyLevelId2',2,'Region'),('hierarchyLevelId3',3,'District'),('hierarchyLevelId4',4,'Village'),('hierarchyLevelId5',5,'Subvillage');
 
 INSERT INTO `privilege` VALUES ('PRIVILEGE1','CREATE_ENTITY'),('PRIVILEGE10','ACCESS_REPORTS'),('PRIVILEGE11','ACESSS_UTILITY_ROUTINES'),('PRIVILEGE12','ACESSS_CONFIGURATION'),('PRIVILEGE2','EDIT_ENTITY'),('PRIVILEGE3','DELETE_ENTITY'),('PRIVILEGE4','VIEW_ENTITY'),('PRIVILEGE5','CREATE_USER'),('PRIVILEGE6','DELETE_USER'),('PRIVILEGE7','ACCESS_BASELINE'),('PRIVILEGE8','ACCESS_UPDATE'),('PRIVILEGE9','ACCESS_AMENDMENT_FORMS');
 
-INSERT INTO `role` VALUES ('ROLE1','\0',NULL,NULL,NULL,'Administrator of OpenHDS','ADMINISTRATOR',NULL,NULL),('ROLE2','\0',NULL,NULL,NULL,'Data Clerk of OpenHDS','DATA CLERK',NULL,NULL),('ROLE3','\0',NULL,NULL,NULL,'Data Manager of OpenHDS','DATA MANAGER',NULL,NULL);
-
+INSERT INTO `role` VALUES ('ROLE1',0,NULL,NULL,NULL,'Administrator of OpenHDS','ADMINISTRATOR',NULL,NULL),('ROLE2',0,NULL,NULL,NULL,'Data Clerk of OpenHDS','DATA CLERK',NULL,NULL),('ROLE3',0,NULL,NULL,NULL,'Data Manager of OpenHDS','DATA MANAGER',NULL,NULL);
 INSERT INTO `role_privileges` VALUES ('ROLE1','PRIVILEGE1'),('ROLE1','PRIVILEGE10'),('ROLE1','PRIVILEGE11'),('ROLE1','PRIVILEGE12'),('ROLE1','PRIVILEGE2'),('ROLE1','PRIVILEGE3'),('ROLE1','PRIVILEGE4'),('ROLE2','PRIVILEGE4'),('ROLE1','PRIVILEGE5'),('ROLE1','PRIVILEGE6'),('ROLE1','PRIVILEGE7'),('ROLE1','PRIVILEGE8'),('ROLE1','PRIVILEGE9');
 
-INSERT INTO `user` VALUES ('User 1','\0','Administrator User','FirstName','Administrator',1415184017493,'LastName','test','7A229A3D44ABEB1D9C5AAB64AFFB1B16','admin');
-INSERT INTO `user` VALUES ('ff8081815462747d01546276bc260000',0,'','Data',NULL,1462956801466,'Data','data','D6269297BDC64C540E04C09C9916C37E','data')
+INSERT INTO `user` VALUES ('ff8081815462747d01546276bc260000',0,'','Data',NULL,1462956801466,'Data','data','D6269297BDC64C540E04C09C9916C37E','data'),('User 1',0,'Administrator User','FirstName','Administrator',1461940806783,'LastName','test','3125A98D0E3564B4E15D940F7CEDDC1A','admin');
 
 INSERT INTO `user_roles` VALUES ('User 1','ROLE1');
 INSERT INTO `whitelist` VALUES ('LOCALHOST1','127.0.0.1'),('LOCALHOST2','localhost');
@@ -62,6 +59,6 @@ SET FOREIGN_KEY_CHECKS=1;
 
 use hrs_mysql_db;
 UPDATE location SET lowestLocationId='ISE' LIMIT 190;
-UPDATE location SET lowestLocationId='GTH' WHERE subvillageId IS NULL LIMIT 190;
-UPDATE location SET lowestLocationId='BCL' WHERE subvillageId IS NULL LIMIT 190;
-UPDATE location SET lowestLocationId='GND' WHERE subvillageId IS NULL LIMIT 192;
+UPDATE location SET lowestLocationId='GTH' WHERE lowestLocationId IS NULL LIMIT 190;
+UPDATE location SET lowestLocationId='BCL' WHERE lowestLocationId IS NULL LIMIT 190;
+UPDATE location SET lowestLocationId='GND' WHERE lowestLocationId IS NULL;
