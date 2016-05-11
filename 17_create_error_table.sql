d7d4630d0cb761bccd1657c862dd06e925bbd29c
Nicolas Maire <nicolas.maire@unibas.ch>
2016-05-11 10:46:18 +0200

    updated error table

--- 17_create_error_table.sql
+++ 17_create_error_table.sql
@@ -1,13 +1,13 @@
 DROP table if exists `errors`;
+
+
 CREATE TABLE `errors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `CHANNEL` varchar(30) DEFAULT NULL,
-  `DATA` varchar(500) DEFAULT NULL,
-  `ERROR` varchar(250) DEFAULT NULL,
+  `DATA` varchar(1000) DEFAULT NULL,
+  `ERROR` varchar(280) DEFAULT NULL,
+  `exported` int(1) DEFAULT '0',
+  `inserted_timestamp` timestamp DEFAULT CURRENT_TIMESTAMP,
+ `COMMENT` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=119 DEFAULT CHARSET=latin1;
-
-
-
-
-
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
